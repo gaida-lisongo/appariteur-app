f04b1a553b7f404c27c8748d4c067a2b30f3e2fc
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-04-23 19:35:41 +0200

    fixe serveur

--- src/services/index.tsx
+++ src/services/index.tsx
@@ -1,6 +1,6 @@
 import AppariteurService from "./appariteur.services";
 
-const API_URL = 'https://back-etudiant-app-z1bz.onrender.com/api'; // Remplacez par l'URL de votre API
+const API_URL = 'https://inbtp-server.onrender.com/api'; // Remplacez par l'URL de votre API
 
 export default {
     Appariteur : new AppariteurService(API_URL),
